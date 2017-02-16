ca0458eda2db0d94c9be09c8aa229508e93e5af1
kdatta <kushal.datta@intel.com>
2017-02-15 18:45:02 -0800

    new vid mapper derived class for reading vid maps directly from protocol buffer based configuration in C++.

---
+++ src/main/cpp/include/utils/vid_mapper.pb.h
@@ -0,0 +1,26 @@
+#ifndef GENOMICSDB_VID_MAPPER_PB_H
+#define #ifndef GENOMICSDB_VID_MAPPER_PB_H
+
+#include "vid_mapper.h"
+
+class ProtoBufBasedVidMapper : public VidMapper {
+  public:
+    ProtoBufBasedVidMapper();
+    ~ProtoBufBasedVidMapper();
+  protected:
+    std::string m_msg;
+};
+
+class ProtoBufBasedVidMapperException : public std::exception {
+  public:
+    ProtoBufBasedVidMapperException(const std::string m="")
+      : msg_("ProtoBufBasedVidMapperException : "+m) { ; }
+    ~ProtoBufBasedVidMapperException() { ; }
+    // ACCESSORS
+    /** Returns the exception message. */
+    const char* what() const noexcept { return msg_.c_str(); }
+  private:
+    std::string msg_;
+};
+
+#endif  // GENOMICSDB_VID_MAPPER_PB_H
