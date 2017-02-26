59237ae9ee54b3a8568ae73646b724786ddc4696
kdatta <kushal.datta@intel.com>
2017-02-25 23:30:50 -0800

    added ProtoBufBasedVidMapper class which maps fields of vidmap protocol buffer to VidMapper

--- src/main/cpp/include/utils/vid_mapper_pb.h
+++ src/main/cpp/include/utils/vid_mapper_pb.h
@@ -2,13 +2,62 @@
 #define GENOMICSDB_VID_MAPPER_PB_H
 
 #include "vid_mapper.h"
+#include "genomicsdb_vid_mapping.pb.h"
+#include "genomicsdb_callsets_mapping.pb.h"
+
+enum {
+  GENOMICSDB_VID_MAPPER_SUCCESS = 0x0,
+  GENOMICSDB_VID_MAPPER_FAILURE = 0x1
+};
 
 class ProtoBufBasedVidMapper : public VidMapper {
   public:
-    ProtoBufBasedVidMapper();
+
+    /**
+     * Constructor
+     * 1. Callset is required. This is not an option (compared to
+     *    FileBasedVidMapper
+     * 2. VidMapping protocol buffer structure contains the
+     *    merged header from all input GVCFs
+     * 3. CallsetMap protocl buffer structure contains the
+     *    callset to TileDB row mapping. It also contains
+     *    stream names. Stream names cannot be empty
+     */
+    ProtoBufBasedVidMapper(
+      const VidMapping&,
+      const CallsetMap&);
+
+    /**
+     * Destructor -- must be called once you allocate an object
+     */
     ~ProtoBufBasedVidMapper();
 
-    void initialize();
+    /**
+     * Initialization routine to populate this jurassic VidMapper
+     * data structure. Super important for maintaining correctness
+     * while filling the underlying TileDB array
+     */
+    void initialize(
+      const VidMapping&,
+      const CallsetMap&);
+
+    /**
+     * Parse the callset map protocol buffer structure and
+     * populate data structures of the base jurassic VidMapper
+     * class
+     */
+    int parse_callset_protobuf(const CallsetMap&);
+
+    /**
+     * Parse the variant id map protocol buffer structure which
+     * contains the merged header. These headers are picked
+     * from each of the input GVCF files. Populate the data
+     * structures of the base jurassic VidMapper class
+     */
+    int parse_vidmap_protobuf(const VidMapping& callsetMapProto);
+    int parse_contigs_from_vidmap(const VidMapping& vidMapProto);
+    int parse_infofields_from_vidmap(const VidMapping& vidMapProto);
+
   protected:
     std::string m_msg;
 };
--- src/main/cpp/src/utils/vid_mapper_pb.cc
+++ src/main/cpp/src/utils/vid_mapper_pb.cc
@@ -1,16 +1,362 @@
 
 #include "vid_mapper_pb.h"
+#include "known_field_info.h"
+#include <ctype.h>
+#include <cstdlib>
+#include <cstdio>
 
-ProtoBufBasedVidMapper::ProtoBufBasedVidMapper()
+#define VERIFY_OR_THROW(X) if(!(X)) throw ProtoBufBasedVidMapperException(#X);
+
+ProtoBufBasedVidMapper::ProtoBufBasedVidMapper(
+  const VidMapping& vidMapProto,
+  const CallsetMap& callsetMapProto
+  )
   : VidMapper() {
 
-  initialize();
+  // Verify that the version of the library that we linked against is
+  // compatible with the version of the headers we compiled against.
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+  initialize(vidMapProto, callsetMapProto);
 }
 
-void ProtoBufBasedVidMapper::initialize() {
-//  m_lb_callset_row_idx = 0;
-//  m_ub_callset_row_idx = INT64_MAX-1;
+void ProtoBufBasedVidMapper::initialize(
+  const VidMapping& vidMapProto,
+  const CallsetMap& callsetMapProto) {
+
+  int ret = 0;
+  ret = parse_callset_protobuf(callsetMapProto);
+  assert (ret == GENOMICSDB_VID_MAPPER_SUCCESS);
+  ret = parse_vidmap_protobuf(vidMapProto);
+  assert (ret == GENOMICSDB_VID_MAPPER_SUCCESS);
+
+  m_is_initialized = true;
 }
+
+int ProtoBufBasedVidMapper::parse_callset_protobuf(
+  const CallsetMap& callsetMapProto) {
+
+  auto num_callsets = callsetMapProto.callset_map_size();
+  m_row_idx_to_info.resize(num_callsets);
+  m_max_callset_row_idx = -1;
+
+  for (int i = 0; i < callsetMapProto.callset_map_size(); ++i) {
+    SampleIDToTileDBIDMap sample_info = callsetMapProto.callset_map(i);
+    std::string callset_name = sample_info.sample_name();
+    int64_t row_idx = sample_info.tiledb_row_index();
+    int64_t sample_vcf_index = sample_info.sample_vcf_index();
+    std::string stream_name = sample_info.stream_name();
+
+    if(m_callset_name_to_row_idx.find(callset_name) !=
+        m_callset_name_to_row_idx.end()) {
+
+      if(m_callset_name_to_row_idx[callset_name] != row_idx) {
+        throw ProtoBufBasedVidMapperException(
+            std::string("ERROR: Callset/sample ")
+            + callset_name
+            + " have two TileDB row indexes: "
+            + std::to_string(m_callset_name_to_row_idx[callset_name])
+            + ", "
+            + std::to_string(row_idx));
+      }
+    }
+
+    m_max_callset_row_idx = std::max(m_max_callset_row_idx, row_idx);
+
+    auto file_idx = get_or_append_global_file_idx(stream_name);
+    const auto& curr_row_info = m_row_idx_to_info[row_idx];
+
+    // If row information for the same callset is found,
+    // check whether it conflicts with current information.
+    // If so, throw error
+    if(curr_row_info.m_is_initialized) {
+      if(curr_row_info.m_file_idx >= 0 && file_idx >= 0
+          && curr_row_info.m_file_idx != file_idx)
+        throw ProtoBufBasedVidMapperException(
+          std::string("Callset/sample ")
+          + callset_name
+          + " has multiple stream names: "
+          + m_file_idx_to_info[curr_row_info.m_file_idx].m_name
+          + ", "
+          + stream_name);
+      if(curr_row_info.m_idx_in_file != sample_vcf_index)
+        throw ProtoBufBasedVidMapperException(
+          std::string("Conflicting values of \"sample_vcf_index\" ")
+          + std::string("specified for Callset/sample ")
+          + callset_name
+          + ": "
+          + std::to_string(curr_row_info.m_idx_in_file)
+          + ", "
+          + std::to_string(sample_vcf_index));
+    } else {
+      assert(file_idx < static_cast<int64_t>(m_file_idx_to_info.size()));
+      m_file_idx_to_info[file_idx].add_local_tiledb_row_idx_pair(
+          sample_vcf_index,
+          row_idx);
+    }
+
+    m_callset_name_to_row_idx[callset_name] = row_idx;
+    VERIFY_OR_THROW(static_cast<size_t>(row_idx) < m_row_idx_to_info.size());
+    if(m_row_idx_to_info[row_idx].m_is_initialized &&
+        m_row_idx_to_info[row_idx].m_name != callset_name)
+          throw ProtoBufBasedVidMapperException(
+            std::string("Callset/sample ")
+            + callset_name
+            + " has the same TileDB row index as "
+            + m_row_idx_to_info[row_idx].m_name);
+
+    m_row_idx_to_info[row_idx].set_info(
+      row_idx,
+      callset_name,
+      file_idx,
+      sample_vcf_index);
+  }  // end of for all callsets
+
+  return GENOMICSDB_VID_MAPPER_SUCCESS;
+} // end of parse_callset_protobuf
+
+int ProtoBufBasedVidMapper::parse_vidmap_protobuf(
+  const VidMapping& vidMapProto) {
+
+  int ret = 0;
+  ret = parse_contigs_from_vidmap(vidMapProto);
+  assert (ret == GENOMICSDB_VID_MAPPER_SUCCESS);
+  ret = parse_infofields_from_vidmap(vidMapProto);
+  assert (ret == GENOMICSDB_VID_MAPPER_SUCCESS);
+
+  return GENOMICSDB_VID_MAPPER_SUCCESS;
+}
+
+int ProtoBufBasedVidMapper::parse_contigs_from_vidmap(
+  const VidMapping& vidMapProto) {
+
+  auto num_contigs = vidMapProto.chromosomes_size();
+  m_contig_idx_to_info.resize(num_contigs);
+  m_contig_begin_2_idx.resize(num_contigs);
+  m_contig_end_2_idx.resize(num_contigs);
+  auto duplicate_contigs_exist = false;
+  std::string contig_name;
+
+  for (auto contig_idx = 0L; contig_idx < num_contigs; ++contig_idx) {
+    contig_name = vidMapProto.chromosomes(contig_idx).name();
+
+    if(m_contig_name_to_idx.find(contig_name) != m_contig_name_to_idx.end())
+    {
+      std::cerr << "Contig/chromosome name "
+                << contig_name
+                << " appears more than once in vid map\n"
+                << " Please check merge headers method "
+                << "in GenomicsDBImporter.java\n";
+      duplicate_contigs_exist = true;
+      continue;
+    }
+
+    auto tiledb_column_offset =
+        vidMapProto.chromosomes(contig_idx).tiledb_column_offset();
+    VERIFY_OR_THROW(tiledb_column_offset >= 0LL);
+    auto length = vidMapProto.chromosomes(contig_idx).length();
+    VERIFY_OR_THROW(length >= 0LL);
+    VERIFY_OR_THROW(static_cast<size_t>(contig_idx) <
+                     static_cast<size_t>(num_contigs));
+    m_contig_name_to_idx[contig_name] = contig_idx;
+    m_contig_idx_to_info[contig_idx].set_info(
+                                       contig_idx,
+                                       contig_name,
+                                       length,
+                                       tiledb_column_offset);
+    m_contig_begin_2_idx[contig_idx].first = tiledb_column_offset;
+    m_contig_begin_2_idx[contig_idx].second = contig_idx;
+    m_contig_end_2_idx[contig_idx].first =
+        tiledb_column_offset + length - 1; //inclusive
+    m_contig_end_2_idx[contig_idx].second = contig_idx;
+
+    if(duplicate_contigs_exist) {
+        throw ProtoBufBasedVidMapperException(
+          std::string("Duplicate contigs found: ")
+          + contig_name);
+    }
+
+    std::sort(
+        m_contig_begin_2_idx.begin(),
+        m_contig_begin_2_idx.end(),
+        contig_offset_idx_pair_cmp);
+    std::sort(
+        m_contig_end_2_idx.begin(),
+        m_contig_end_2_idx.end(),
+        contig_offset_idx_pair_cmp);
+  }
+
+  // Check that there are no spurious overlaps.
+  // If found, throw an exception
+  auto last_contig_idx = -1;
+  auto last_contig_end_column = -1ll;
+  auto overlapping_contigs_exist = false;
+  for(const auto& offset_idx_pair : m_contig_begin_2_idx)
+  {
+    auto contig_idx = offset_idx_pair.second;
+    const auto& contig_info = m_contig_idx_to_info[contig_idx];
+    if(last_contig_idx >= 0)
+    {
+      const auto& last_contig_info = m_contig_idx_to_info[last_contig_idx];
+      if(contig_info.m_tiledb_column_offset <= last_contig_end_column)
+      {
+        std::cerr << "Contig/chromosome "
+                  << contig_info.m_name
+                  << " begins at TileDB column "
+                  << contig_info.m_tiledb_column_offset
+                  << " and intersects with contig/chromosome "
+                  << last_contig_info.m_name
+                  << " that spans columns ["
+                  << last_contig_info.m_tiledb_column_offset
+                  << ", "
+                  << last_contig_info.m_tiledb_column_offset +
+                     last_contig_info.m_length-1
+                  << "]" << "\n";
+        overlapping_contigs_exist = true;
+      }
+    }
+    last_contig_idx = contig_idx;
+    last_contig_end_column =
+        contig_info.m_tiledb_column_offset + contig_info.m_length - 1;
+  }
+
+  if(overlapping_contigs_exist) {
+    throw ProtoBufBasedVidMapperException(
+      std::string("Overlapping contigs found"));
+  }
+
+  return GENOMICSDB_VID_MAPPER_SUCCESS;
+} // end of parse_contigs_from_vidmap
+
+int ProtoBufBasedVidMapper::parse_infofields_from_vidmap(
+  const VidMapping& vidMapProto) {
+
+  auto num_fields = vidMapProto.infofields_size();
+  m_field_idx_to_info.resize(num_fields);
+  std::string field_name;
+  std::string field_type;
+  auto duplicate_fields_exist = false;
+
+  for (auto field_idx = 0; field_idx < num_fields; ++field_idx) {
+    field_name = vidMapProto.infofields(field_idx).name();
+    if(m_field_name_to_idx.find(field_name) != m_field_name_to_idx.end()) {
+      std::cerr << "Duplicate field name "
+                << field_name
+                << " found in vid map\n";
+      duplicate_fields_exist = true;
+      continue;
+    }
+
+    // Known fields
+    auto known_field_enum = 0u;
+    auto is_known_field =
+        KnownFieldInfo::get_known_field_enum_for_name(
+          field_name,
+          known_field_enum);
+    // Map
+    m_field_name_to_idx[field_name] = field_idx;
+    m_field_idx_to_info[field_idx].set_info(field_name, field_idx);
+
+    //Field type - int, char etc
+    field_type = vidMapProto.infofields(field_idx).type();
+    {
+      auto iter = VidMapper::m_typename_string_to_type_index.find(field_type);
+      VERIFY_OR_THROW(iter != VidMapper::m_typename_string_to_type_index.end()
+          && "Field type not handled");
+      m_field_idx_to_info[field_idx].m_type_index = (*iter).second;
+    }
+    {
+      auto iter = VidMapper::m_typename_string_to_bcf_ht_type.find(field_type);
+      VERIFY_OR_THROW(iter != VidMapper::m_typename_string_to_bcf_ht_type.end()
+          && "Field type not handled");
+      m_field_idx_to_info[field_idx].m_bcf_ht_type = (*iter).second;
+    }
+
+    // VCF class type can be an array of values: INFO, FORMAT and FILTER
+    auto class_type_size =
+        vidMapProto.infofields(field_idx).vcf_field_class_type_size();
+
+    if (class_type_size > 0L) {
+      for (int i = 0; i < class_type_size; ++i) {
+        std::string class_name = vidMapProto.infofields(field_idx).name();
+        if(class_name == "INFO")
+          m_field_idx_to_info[field_idx].m_is_vcf_INFO_field = true;
+        else if(class_name == "FORMAT")
+          m_field_idx_to_info[field_idx].m_is_vcf_FORMAT_field = true;
+        else
+          if(class_name == "FILTER")
+            m_field_idx_to_info[field_idx].m_is_vcf_FILTER_field = true;
+      }
+    }
+
+    if (vidMapProto.infofields(field_idx).has_length()) {
+      std::string length = vidMapProto.infofields(field_idx).length();
+      if (isdigit(length.c_str()[0])) {
+        m_field_idx_to_info[field_idx].m_num_elements =
+            strtol(length.c_str(), NULL, 10);
+      } else {
+        auto iter = VidMapper::m_length_descriptor_string_to_int.find(length);
+        if (iter == VidMapper::m_length_descriptor_string_to_int.end()) {
+          m_field_idx_to_info[field_idx].m_length_descriptor = BCF_VL_VAR;
+        } else {
+          m_field_idx_to_info[field_idx].m_length_descriptor = (*iter).second;
+        }
+      }
+    } else {
+      if(is_known_field) {
+        auto length_descriptor =
+            KnownFieldInfo::get_length_descriptor_for_known_field_enum(
+                known_field_enum);
+        m_field_idx_to_info[field_idx].m_length_descriptor = length_descriptor;
+        if(length_descriptor == BCF_VL_FIXED)
+          m_field_idx_to_info[field_idx].m_num_elements =
+              KnownFieldInfo::get_num_elements_for_known_field_enum(
+                  known_field_enum,
+                  0u,
+                  0u);  //don't care about ploidy
+      }
+    }
+
+    // Both INFO and FORMAT, throw another entry <field>_FORMAT
+    if (m_field_idx_to_info[field_idx].m_is_vcf_INFO_field &&
+        m_field_idx_to_info[field_idx].m_is_vcf_FORMAT_field) {
+      auto new_field_idx = field_idx+1u;
+      m_field_idx_to_info.resize(m_field_idx_to_info.size()+1u);
+      //Copy field information
+      m_field_idx_to_info[new_field_idx] = m_field_idx_to_info[field_idx];
+      auto& new_field_info =  m_field_idx_to_info[new_field_idx];
+      //Update name and index - keep the same VCF name
+      new_field_info.m_name = field_name+"_FORMAT";
+      new_field_info.m_is_vcf_INFO_field = false;
+      new_field_info.m_field_idx = new_field_idx;
+      new_field_info.m_VCF_field_combine_operation =
+          VCFFieldCombineOperationEnum::VCF_FIELD_COMBINE_OPERATION_UNKNOWN_OPERATION;
+      //Update map
+      m_field_name_to_idx[new_field_info.m_name] = new_field_idx;
+      //Set FORMAT to false for original field
+      m_field_idx_to_info[field_idx].m_is_vcf_FORMAT_field = false;
+    }
+  } // for (auto field_idx = 0; field_idx < num_fields; ++field_idx)
+
+  //Force add END as a field
+  auto iter = m_field_name_to_idx.find("END");
+  if(iter == m_field_name_to_idx.end()) {
+    auto end_idx = m_field_idx_to_info.size();
+    m_field_idx_to_info.emplace_back();
+    m_field_name_to_idx["END"] = end_idx;
+    auto& field_info = m_field_idx_to_info[end_idx];
+    field_info.set_info("END", end_idx);
+    field_info.m_is_vcf_INFO_field = true;
+    field_info.m_type_index = std::move(std::type_index(typeid(int)));
+    field_info.m_bcf_ht_type = BCF_HT_INT;
+  }
+  if(duplicate_fields_exist) {
+    throw ProtoBufBasedVidMapperException(
+      std::string("Duplicate fields exist in vid map"));
+  }
+
+  return GENOMICSDB_VID_MAPPER_SUCCESS;
+} // end of parse_infofields_from_vidmap
 
 ProtoBufBasedVidMapperException::~ProtoBufBasedVidMapperException() { ; }
 
