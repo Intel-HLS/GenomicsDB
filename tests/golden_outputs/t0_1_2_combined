Intel-HLS/GenomicsDB

commit d260bf63ae517b5e1c9b3b87556c0ce794a72ac3

Fixed CSV export and import functionality Can direct binary output of all callsets within a file to a single buffer using the 'order' variable. Can re-use that idea for the multi node implementation. Assign designated row idx to each order value

src/loader/tiledb_loader.cc
@@ -273,7 +273,20 @@ void VCF2TileDBConverter::initialize_column_batch_objects()
   int64_t order = 0ll;
   for(const auto& x : m_file2binary_handlers)
     x->set_order_of_enabled_callsets(order, m_tiledb_row_idx_to_order);
-  assert(order == m_num_callsets_owned);
+  //Could be less if multiple callsets point to the same order - this happens for CSV files with multiple callsets
+  assert(order <= m_num_callsets_owned);
+  m_order_to_designated_tiledb_row_idx = std::move(std::vector<int64_t>(order, -1ll));
+  //Assign smallest row idx to each order value
+  for(auto i=0ull;i<m_tiledb_row_idx_to_order.size();++i)
+  {
+    auto order = m_tiledb_row_idx_to_order[i];
+    if(order >= 0)
+    {
+      assert(static_cast<size_t>(order) < m_order_to_designated_tiledb_row_idx.size());
+      if(m_order_to_designated_tiledb_row_idx[order] < 0)
+        m_order_to_designated_tiledb_row_idx[order] = i;
+    }
+  }
 }
 
 void VCF2TileDBConverter::activate_next_batch(const unsigned exchange_idx, const int partition_idx)
@@ -419,37 +432,37 @@ VCF2TileDBLoader::VCF2TileDBLoader(const std::string& config_filename, int idx)
     //Allocate exchange objects
   }
   else
-  {
-    //Allocate exchange objects
-    for(auto& x : m_owned_exchanges)
-    {
-      x.initialize_from_loader(m_num_callsets_owned);
-      x.m_all_num_tiledb_row_idx_vec_request[0] = m_num_callsets_owned;
-      for(auto i=0ll;i<m_num_callsets_owned;++i)
-      {
-        assert(static_cast<size_t>(i) < m_owned_row_idx_vec.size());
-        x.m_all_tiledb_row_idx_vec_request[x.get_idx_offset_for_converter(0)+i] = m_owned_row_idx_vec[i];
-      }
-    }
+  { 
 #ifdef HTSDIR
     m_converter = new VCF2TileDBConverter(config_filename, idx, m_vid_mapper, &m_ping_pong_buffers, &m_owned_exchanges);
 #endif
+    //Num order values
+    auto num_order_values = get_num_order_values();
+    //Initialize exchange objects
+    for(auto& x : m_owned_exchanges)
+    {
+      x.initialize_from_loader(num_order_values);
+      x.m_all_num_tiledb_row_idx_vec_request[0] = num_order_values;
+      for(auto i=0ull;i<num_order_values;++i)
+        x.m_all_tiledb_row_idx_vec_request[x.get_idx_offset_for_converter(0)+i] = get_designated_row_idx_for_order(i);
+    }
   }
   //Allocate buffers
   for(auto i=0u;i<m_ping_pong_buffers.size();++i)
     m_ping_pong_buffers[i].resize(m_per_partition_size);
+  //Num order values
+  auto num_order_values = get_num_order_values();
   //Circular buffer control
-  m_order_idx_to_buffer_control.resize(m_num_callsets_owned, CircularBufferController(m_num_entries_in_circular_buffer));
+  m_order_idx_to_buffer_control.resize(num_order_values, CircularBufferController(m_num_entries_in_circular_buffer));
   //Priority queue elements
-  m_pq_vector.resize(m_num_callsets_owned);
-  m_rows_not_in_pq.resize(m_num_callsets_owned);
-  for(const auto row_idx : m_owned_row_idx_vec)
+  m_pq_vector.resize(num_order_values);
+  m_designated_rows_not_in_pq.resize(num_order_values);
+  for(auto order=0ull;order<num_order_values;++order)
   {
-    auto order = get_order_for_row_idx(row_idx);
-    assert(order >= 0 && static_cast<size_t>(order) < m_order_idx_to_buffer_control.size());
+    auto row_idx = get_designated_row_idx_for_order(order);
     m_pq_vector[order].m_row_idx = row_idx;
     m_pq_vector[order].m_offset = get_buffer_start_offset_for_row_idx(row_idx);
-    m_rows_not_in_pq[order] = row_idx;
+    m_designated_rows_not_in_pq[order] = row_idx;
   }
 #ifdef PRODUCE_BINARY_CELLS
   if(m_produce_combined_vcf)
@@ -589,15 +602,14 @@ bool VCF2TileDBLoader::dump_latest_buffer(unsigned exchange_idx, std::ostream& o
     for(auto i=0ll;i<curr_exchange.m_all_num_tiledb_row_idx_vec_response[converter_idx];++i)
     {
       auto row_idx = curr_exchange.m_all_tiledb_row_idx_vec_response[idx_offset+i];
-      assert(row_idx >= 0 && static_cast<size_t>(row_idx) < m_order_idx_to_buffer_control.size());
+      auto order = get_order_for_row_idx(row_idx);
+      assert(order >= 0 && static_cast<size_t>(order) < m_order_idx_to_buffer_control.size());
       //Add to next request
       curr_exchange.m_all_tiledb_row_idx_vec_request[idx_offset + i] = row_idx;
       //Ping pong buffering control
-      auto buffer_idx = m_order_idx_to_buffer_control[row_idx].get_read_idx();
-      assert(m_order_idx_to_buffer_control[row_idx].get_num_entries_with_valid_data() > 0u);
-      auto order = get_order_for_row_idx(row_idx);
-      assert(order >= 0);
-      int64_t offset = order*m_max_size_per_callset;
+      auto buffer_idx = m_order_idx_to_buffer_control[order].get_read_idx();
+      assert(m_order_idx_to_buffer_control[order].get_num_entries_with_valid_data() > 0u);
+      int64_t offset = get_buffer_start_offset_for_row_idx(row_idx);
       auto j=0ll;
       for(;j<m_max_size_per_callset;++j)
       {
@@ -607,7 +619,7 @@ bool VCF2TileDBLoader::dump_latest_buffer(unsigned exchange_idx, std::ostream& o
         else
           break;
       }
-      m_order_idx_to_buffer_control[row_idx].advance_read_idx();
+      m_order_idx_to_buffer_control[order].advance_read_idx();
     }
     curr_exchange.m_all_num_tiledb_row_idx_vec_request[converter_idx] = curr_exchange.m_all_num_tiledb_row_idx_vec_response[converter_idx];
     return (curr_exchange.m_all_num_tiledb_row_idx_vec_response[converter_idx] == 0);
@@ -636,7 +648,9 @@ bool VCF2TileDBLoader::read_cell_from_buffer(const int64_t row_idx)
   //row idx in buffer == NULL, no more valid data
   if(row_idx_in_buffer == get_tiledb_null_value<int64_t>())
     return false;
-  assert(row_idx_in_buffer == row_idx);
+  //Must point to the same order value
+  assert(get_order_for_row_idx(row_idx_in_buffer) == order);
+  pq_element.m_row_idx = row_idx_in_buffer;
   pq_element.m_column = ptr[1];
   return true;
 }
@@ -678,13 +692,13 @@ bool VCF2TileDBLoader::produce_cells_in_column_major_order(unsigned exchange_idx
   auto converter_idx = 0u;
   auto idx_offset = curr_exchange.get_idx_offset_for_converter(converter_idx);
   //Add callsets that are not in PQ into the PQ if valid cells found
-  for(auto i=0ull;i<m_rows_not_in_pq.size();++i)
+  for(auto i=0ull;i<m_designated_rows_not_in_pq.size();++i)
   {
-    auto row_idx = m_rows_not_in_pq[i];
+    auto row_idx = m_designated_rows_not_in_pq[i];
     auto valid_cell_found = read_cell_from_buffer(row_idx);
     auto order = get_order_for_row_idx(row_idx);
     assert(order >= 0 && static_cast<size_t>(order) < m_order_idx_to_buffer_control.size());
-    assert(m_pq_vector[order].m_row_idx == row_idx);
+    assert(get_order_for_row_idx(m_pq_vector[order].m_row_idx) == order);
     if(valid_cell_found)
     {
       m_column_major_pq.push(&(m_pq_vector[order]));
@@ -693,12 +707,13 @@ bool VCF2TileDBLoader::produce_cells_in_column_major_order(unsigned exchange_idx
     else
       m_pq_vector[order].m_completed = true;
   }
-  auto num_rows_not_in_pq = 0ull;
+  auto num_designated_rows_not_in_pq = 0ull;
   //No re-allocation as resize() doesn't reduce capacity
-  m_rows_not_in_pq.resize(m_num_callsets_owned);
+  m_designated_rows_not_in_pq.resize(get_num_order_values());
   auto top_column = -1ll;
   auto hit_invalid_cell = false;
-  while(!m_column_major_pq.empty() && (!hit_invalid_cell || (m_column_major_pq.top())->m_column == top_column))
+  //while(!m_column_major_pq.empty() && (!hit_invalid_cell || (m_column_major_pq.top())->m_column == top_column))
+  while(!m_column_major_pq.empty() && !hit_invalid_cell)
   {
     auto* top_ptr = m_column_major_pq.top();
     m_column_major_pq.pop();
@@ -720,32 +735,26 @@ bool VCF2TileDBLoader::produce_cells_in_column_major_order(unsigned exchange_idx
         hit_invalid_cell = true;
         top_column = top_ptr->m_column;
       }
-      m_rows_not_in_pq[num_rows_not_in_pq++] = row_idx;
+      m_designated_rows_not_in_pq[num_designated_rows_not_in_pq++] = get_designated_row_idx_for_order(order);
     }
   }
   //No re-allocation as resize() doesn't reduce capacity
-  m_rows_not_in_pq.resize(num_rows_not_in_pq);
+  m_designated_rows_not_in_pq.resize(num_designated_rows_not_in_pq);
   //Find rows to request in next batch - rows which have empty space
   auto num_rows_in_next_request = 0ull;
-  for(auto i=0ll;i<m_num_callsets_owned;++i)
+  for(auto order=0ull;order<get_num_order_values();++order)
   {
-    auto& pq_element = m_pq_vector[i];
-    auto row_idx = m_pq_vector[i].m_row_idx;
+    auto& pq_element = m_pq_vector[order];
     pq_element.m_crossed_one_buffer = false;
-#ifdef DEBUG
-    auto order = get_order_for_row_idx(row_idx);
-    assert(order >= 0 && static_cast<size_t>(order) < m_order_idx_to_buffer_control.size());
-    assert(order == i);
-#endif
     //Space in circular buffer
-    if(!pq_element.m_completed && m_order_idx_to_buffer_control[i].get_num_empty_entries() > 0u)
+    if(!pq_element.m_completed && m_order_idx_to_buffer_control[order].get_num_empty_entries() > 0u)
     {
-      curr_exchange.m_all_tiledb_row_idx_vec_request[idx_offset + num_rows_in_next_request] = row_idx;
+      curr_exchange.m_all_tiledb_row_idx_vec_request[idx_offset + num_rows_in_next_request] = get_designated_row_idx_for_order(order);
       ++num_rows_in_next_request;
     }
   }
   curr_exchange.m_all_num_tiledb_row_idx_vec_request[converter_idx] = num_rows_in_next_request;
-  return (m_column_major_pq.empty() && num_rows_in_next_request == 0u && num_rows_not_in_pq == 0u);
+  return (m_column_major_pq.empty() && num_rows_in_next_request == 0u && num_designated_rows_not_in_pq == 0u);
 }
 
 void VCF2TileDBLoader::clear()

include/loader/tiledb_loader.h
@@ -146,6 +146,14 @@ class VCF2TileDBConverter : public VCF2TileDBLoaderConverterBase
       assert(static_cast<size_t>(row_idx) < m_tiledb_row_idx_to_order.size());
       return m_tiledb_row_idx_to_order[row_idx];
     }
+    inline int64_t get_designated_row_idx_for_order(const int64_t order) const
+    {
+      assert(order >= 0 && static_cast<size_t>(order) < m_order_to_designated_tiledb_row_idx.size());
+      auto val = m_order_to_designated_tiledb_row_idx[order];
+      assert(val >= 0 && static_cast<size_t>(val) < m_tiledb_row_idx_to_order.size());
+      return val;
+    }
+    inline size_t get_num_order_values() const { return m_order_to_designated_tiledb_row_idx.size(); }
     void create_and_print_histogram(const std::string& config_filename, std::ostream& fptr=std::cout);
   private:
     void clear();
@@ -160,8 +168,11 @@ class VCF2TileDBConverter : public VCF2TileDBLoaderConverterBase
     std::vector<std::vector<std::string>> m_vcf_fields;
     //One per VCF file
     std::vector<File2TileDBBinaryBase*> m_file2binary_handlers;
+    //Multiple row_idx could point to the same order
     //Ordering of TileDB row idx for this converter - determined by the order of m_file2binary_handlers
     std::vector<int64_t> m_tiledb_row_idx_to_order;
+    //Order to row_idx - points to one designated row_idx for a given order value
+    std::vector<int64_t> m_order_to_designated_tiledb_row_idx;
     //References to ping-pong buffers 
     //May point to buffers owned by this object or by VCF2TileDBLoader depending on the configuration
     std::vector<std::vector<uint8_t>*> m_cell_data_buffers; 
@@ -240,6 +251,22 @@ class VCF2TileDBLoader : public VCF2TileDBLoaderConverterBase
       return row_idx;
 #endif
     }
+    inline int64_t get_designated_row_idx_for_order(const int64_t order) const
+    {
+#ifdef HTSDIR
+      return m_standalone_converter_process ? order : m_converter->get_designated_row_idx_for_order(order);
+#else
+      return order;
+#endif
+    }
+    inline size_t get_num_order_values() const
+    {
+#ifdef HTSDIR
+      return m_standalone_converter_process ? m_num_callsets_owned : m_converter->get_num_order_values();
+#else
+      return m_num_callsets_owned;
+#endif
+    }
     inline int64_t get_buffer_start_offset_for_row_idx(const int64_t row_idx) const
     {
       assert(get_order_for_row_idx(row_idx) >= 0 && get_order_for_row_idx(row_idx) < m_num_callsets_owned);
@@ -268,7 +295,7 @@ class VCF2TileDBLoader : public VCF2TileDBLoaderConverterBase
     std::vector<CellPQElement> m_pq_vector;
     TileDBColumnMajorPQ m_column_major_pq;
     //Row idxs not in PQ - need to be inserted in next call
-    std::vector<int64_t> m_rows_not_in_pq;
+    std::vector<int64_t> m_designated_rows_not_in_pq;
     //Operators - act on one cell per call
     std::vector<LoaderOperatorBase*> m_operators;
 };

include/loader/tiledb_loader_file_base.h
@@ -134,11 +134,11 @@ class File2TileDBBinaryBase
     /*
      * Set order of enabled callsets
      */
-    void set_order_of_enabled_callsets(int64_t& order_value, std::vector<int64_t>& tiledb_row_idx_to_order) const;
+    virtual void set_order_of_enabled_callsets(int64_t& order_value, std::vector<int64_t>& tiledb_row_idx_to_order) const = 0;
     /*
      * List active row idxs
      */
-    void list_active_row_idxs(const ColumnPartitionBatch& partition_batch, int64_t& row_idx_offset, std::vector<int64_t>& row_idx_vec) const;
+    virtual void list_active_row_idxs(const ColumnPartitionBatch& partition_batch, int64_t& row_idx_offset, std::vector<int64_t>& row_idx_vec) const = 0;
     /*
      * */
     void read_next_batch(std::vector<std::vector<uint8_t>*>& buffer_vec,

include/loader/tiledb_loader_text_file.h
@@ -224,6 +224,16 @@ class CSV2TileDBBinary : public LineBasedTextFile2TileDBBinary
     { }
     //Functions that must be over-ridden by all sub-classes
     /*
+     * Set order of enabled callsets
+     * In a CSV, a line contains information from a single callset
+     */
+    void set_order_of_enabled_callsets(int64_t& order_value, std::vector<int64_t>& tiledb_row_idx_to_order) const;
+    /*
+     * List active row idxs
+     * In a CSV, a line contains information from a single callset
+     */
+    void list_active_row_idxs(const ColumnPartitionBatch& partition_batch, int64_t& row_idx_offset, std::vector<int64_t>& row_idx_vec) const;
+    /*
      * Initialization of column partitions by sub class
      */
     void initialize_column_partitions(const std::vector<ColumnRange>& partition_bounds);

include/vcf/vcf2binary.h
@@ -135,6 +135,16 @@ class VCF2Binary : public File2TileDBBinaryBase
     //Initialization functions
     void initialize(const std::vector<ColumnRange>& partition_bounds);
     //Abstract virtual functions in base class that must be defined 
+    /*
+     * Set order of enabled callsets
+     * In VCFs, each line contains data from all callsets
+     */
+    void set_order_of_enabled_callsets(int64_t& order_value, std::vector<int64_t>& tiledb_row_idx_to_order) const;
+    /*
+     * List active row idxs
+     * In VCFs, each line contains data from all callsets
+     */
+    void list_active_row_idxs(const ColumnPartitionBatch& partition_batch, int64_t& row_idx_offset, std::vector<int64_t>& row_idx_vec) const;
     void initialize_column_partitions(const std::vector<ColumnRange>& partition_bounds);
     /*
      * Create the subclass of File2TileDBBinaryColumnPartitionBase that must be used

commit 4caa3afd53ecabad61bfa53d0d1cb72b96e17638

* Fixed bug introduced by d260bf63ae517b when loading data from VCF files containing multiple samples/CallSets * Added CI tests with an input file containing multiple samples/CallSets

src/main/cpp/src/loader/tiledb_loader.cc
@@ -927,8 +927,13 @@ bool VCF2TileDBLoader::produce_cells_in_column_major_order(unsigned exchange_idx
   auto top_column = -1ll;
   auto hit_invalid_cell = false;
   auto num_operators_overflow_in_this_round = 0u;
-  //while(!m_column_major_pq.empty() && (!hit_invalid_cell || (m_column_major_pq.top())->m_column == top_column))
-  while(!m_column_major_pq.empty() && !hit_invalid_cell && num_operators_overflow_in_this_round == 0u)
+  //The more complex condition check is needed to handle the case where a single VCF has multiple samples/callsets
+  //since all samples within a single VCF must be processed together
+  //The clause (m_column_major_pq.top())->m_column == top_column) ensures that all samples from the same VCF are processed
+  //completely the moment one of them causes hit_invalid_cell to be hit. This way either all samples within a VCF are
+  //requested in the next round or none are
+  while(!m_column_major_pq.empty() && (!hit_invalid_cell || (m_column_major_pq.top())->m_column == top_column)
+      && num_operators_overflow_in_this_round == 0u)
   {
     auto* top_ptr = m_column_major_pq.top();
     auto row_idx = top_ptr->m_row_idx;

tests/golden_outputs/t0_1_2_combined
@@ -1,0 +1,123 @@
+##fileformat=VCFv4.1
+##FILTER=<ID=PASS,Description="All filters passed">
+##ALT=<ID=NON_REF,Description="Represents any possible alternative allele at this location">
+##FILTER=<ID=LowQual,Description="Low quality">
+##FORMAT=<ID=AD,Number=R,Type=Integer,Description="Allelic depths for the ref and alt alleles in the order listed">
+##FORMAT=<ID=DP,Number=1,Type=Integer,Description="Approximate read depth (reads with MQ=255 or with bad mates are filtered)">
+##FORMAT=<ID=GQ,Number=1,Type=Integer,Description="Genotype Quality">
+##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+##FORMAT=<ID=MIN_DP,Number=1,Type=Integer,Description="Minimum DP observed within the GVCF block">
+##FORMAT=<ID=PL,Number=G,Type=Integer,Description="Normalized, Phred-scaled likelihoods for genotypes as defined in the VCF specification">
+##FORMAT=<ID=SB,Number=4,Type=Integer,Description="Per-sample component statistics which comprise the Fisher's Exact Test to detect strand bias.">
+##FORMAT=<ID=PGT,Number=1,Type=String,Description="Physical phasing haplotype information, describing how the alternate alleles are phased in relation to one another">
+##FORMAT=<ID=PID,Number=1,Type=String,Description="Physical phasing ID information, where each unique ID within a given sample (but not across samples) connects records within a phasing group">
+##GATKCommandLine=<ID=HaplotypeCaller,Version=3.1-1-g07a4bf8,Date="Fri Apr 04 09:42:24 EDT 2014",Epoch=1396618944211,CommandLineOptions="analysis_type=HaplotypeCaller input_file=[/seq/external-data/1kg/GBR/exome/HG00141/HG00141.bam] showFullBamList=false read_buffer_size=null phone_home=AWS gatk_key=null tag=NA read_filter=[] intervals=[/seq/picardtemp3/seq/sample_vcf/1kg_GBR/Exome/Homo_sapiens_assembly19/7d4759a5-8e11-324c-8d3a-cc375e53e06c/scattered/temp_0001_of_10/scattered.intervals] excludeIntervals=null interval_set_rule=UNION interval_merging=ALL interval_padding=0 reference_sequence=/seq/references/Homo_sapiens_assembly19/v1/Homo_sapiens_assembly19.fasta nonDeterministicRandomSeed=false disableDithering=false maxRuntime=-1 maxRuntimeUnits=MINUTES downsampling_type=BY_SAMPLE downsample_to_fraction=null downsample_to_coverage=250 baq=OFF baqGapOpenPenalty=40.0 fix_misencoded_quality_scores=false allow_potentially_misencoded_quality_scores=false useOriginalQualities=false defaultBaseQualities=-1 performanceLog=null BQSR=null quantize_quals=0 disable_indel_quals=false emit_original_quals=false preserve_qscores_less_than=6 globalQScorePrior=-1.0 validation_strictness=SILENT remove_program_records=false keep_program_records=false sample_rename_mapping_file=null unsafe=null disable_auto_index_creation_and_locking_when_reading_rods=true num_threads=1 num_cpu_threads_per_data_thread=1 num_io_threads=0 monitorThreadEfficiency=false num_bam_file_handles=null read_group_black_list=null pedigree=[] pedigreeString=[] pedigreeValidationType=STRICT allow_intervals_with_unindexed_bam=false generateShadowBCF=false variant_index_type=LINEAR variant_index_parameter=128000 logging_level=INFO log_to_file=null help=false version=false likelihoodCalculationEngine=PairHMM heterogeneousKmerSizeResolution=COMBO_MIN graphOutput=null bamOutput=null bam_compression=null disable_bam_indexing=null generate_md5=null simplifyBAM=null bamWriterType=CALLED_HAPLOTYPES dbsnp=(RodBinding name= source=UNBOUND) dontTrimActiveRegions=false maxDiscARExtension=25 maxGGAARExtension=300 paddingAroundIndels=150 paddingAroundSNPs=20 comp=[] annotation=[ClippingRankSumTest, DepthPerSampleHC, StrandBiasBySample] excludeAnnotation=[SpanningDeletions, TandemRepeatAnnotator, ChromosomeCounts, FisherStrand, QualByDepth] heterozygosity=0.001 indel_heterozygosity=1.25E-4 genotyping_mode=DISCOVERY standard_min_confidence_threshold_for_calling=-0.0 standard_min_confidence_threshold_for_emitting=-0.0 alleles=(RodBinding name= source=UNBOUND) max_alternate_alleles=3 input_prior=[] contamination_fraction_to_filter=0.019 contamination_fraction_per_sample_file=null p_nonref_model=EXACT_INDEPENDENT exactcallslog=null kmerSize=[10, 25] dontIncreaseKmerSizesForCycles=false numPruningSamples=1 recoverDanglingHeads=false dontRecoverDanglingTails=false consensus=false emitRefConfidence=GVCF GVCFGQBands=[5, 20, 60] indelSizeToEliminateInRefModel=10 min_base_quality_score=10 minPruning=3 gcpHMM=10 includeUmappedReads=false useAllelesTrigger=false useFilteredReadsForAnnotations=false phredScaledGlobalReadMismappingRate=45 maxNumHaplotypesInPopulation=200 mergeVariantsViaLD=false pair_hmm_implementation=VECTOR_LOGLESS_CACHING keepRG=null justDetermineActiveRegions=false dontGenotype=false errorCorrectKmers=false debug=false debugGraphTransformations=false dontUseSoftClippedBases=false captureAssemblyFailureBAM=false allowCyclesInKmerGraphToGeneratePaths=false noFpga=false errorCorrectReads=false kmerLengthForReadErrorCorrection=25 minObservationsForKmerToBeSolid=20 pcr_indel_model=CONSERVATIVE activityProfileOut=null activeRegionOut=null activeRegionIn=null activeRegionExtension=null forceActive=false activeRegionMaxSize=null bandPassSigma=null min_mapping_quality_score=20 filter_reads_with_N_cigar=false filter_mismatching_base_and_quals=false filter_bases_not_stored=false">
+##GVCFBlock=minGQ=0(inclusive),maxGQ=5(exclusive)
+##GVCFBlock=minGQ=20(inclusive),maxGQ=60(exclusive)
+##GVCFBlock=minGQ=5(inclusive),maxGQ=20(exclusive)
+##GVCFBlock=minGQ=60(inclusive),maxGQ=2147483647(exclusive)
+##INFO=<ID=BaseQRankSum,Number=1,Type=Float,Description="Z-score from Wilcoxon rank sum test of Alt Vs. Ref base qualities">
+##INFO=<ID=ClippingRankSum,Number=1,Type=Float,Description="Z-score From Wilcoxon rank sum test of Alt vs. Ref number of hard clipped bases">
+##INFO=<ID=DP,Number=1,Type=Integer,Description="Approximate read depth; some reads may have been filtered">
+##INFO=<ID=DS,Number=0,Type=Flag,Description="Were any of the samples downsampled?">
+##INFO=<ID=END,Number=1,Type=Integer,Description="Stop position of the interval">
+##INFO=<ID=HaplotypeScore,Number=1,Type=Float,Description="Consistency of the site with at most two segregating haplotypes">
+##INFO=<ID=InbreedingCoeff,Number=1,Type=Float,Description="Inbreeding coefficient as estimated from the genotype likelihoods per-sample when compared against the Hardy-Weinberg expectation">
+##INFO=<ID=MLEAC,Number=A,Type=Integer,Description="Maximum likelihood expectation (MLE) for the allele counts (not necessarily the same as the AC), for each ALT allele, in the same order as listed">
+##INFO=<ID=MLEAF,Number=A,Type=Float,Description="Maximum likelihood expectation (MLE) for the allele frequency (not necessarily the same as the AF), for each ALT allele, in the same order as listed">
+##INFO=<ID=MQ,Number=1,Type=Float,Description="RMS Mapping Quality">
+##INFO=<ID=RAW_MQ,Number=1,Type=Float,Description="Raw data for RMS Mapping Quality">
+##INFO=<ID=MQ0,Number=1,Type=Integer,Description="Total Mapping Quality Zero Reads">
+##INFO=<ID=MQRankSum,Number=1,Type=Float,Description="Z-score From Wilcoxon rank sum test of Alt vs. Ref read mapping qualities">
+##INFO=<ID=ReadPosRankSum,Number=1,Type=Float,Description="Z-score from Wilcoxon rank sum test of Alt vs. Ref read position bias">
+##reference=file:///seq/references/Homo_sapiens_assembly19/v1/Homo_sapiens_assembly19.fasta
+##contig=<ID=1,length=249250621>
+##contig=<ID=2,length=243199373>
+##contig=<ID=3,length=198022430>
+##contig=<ID=4,length=191154276>
+##contig=<ID=5,length=180915260>
+##contig=<ID=6,length=171115067>
+##contig=<ID=7,length=159138663>
+##contig=<ID=8,length=146364022>
+##contig=<ID=9,length=141213431>
+##contig=<ID=10,length=135534747>
+##contig=<ID=11,length=135006516>
+##contig=<ID=12,length=133851895>
+##contig=<ID=13,length=115169878>
+##contig=<ID=14,length=107349540>
+##contig=<ID=15,length=102531392>
+##contig=<ID=16,length=90354753>
+##contig=<ID=17,length=81195210>
+##contig=<ID=18,length=78077248>
+##contig=<ID=19,length=59128983>
+##contig=<ID=20,length=63025520>
+##contig=<ID=21,length=48129895>
+##contig=<ID=22,length=51304566>
+##contig=<ID=X,length=155270560>
+##contig=<ID=Y,length=59373566>
+##contig=<ID=MT,length=16569>
+##contig=<ID=GL000207.1,length=4262>
+##contig=<ID=GL000226.1,length=15008>
+##contig=<ID=GL000229.1,length=19913>
+##contig=<ID=GL000231.1,length=27386>
+##contig=<ID=GL000210.1,length=27682>
+##contig=<ID=GL000239.1,length=33824>
+##contig=<ID=GL000235.1,length=34474>
+##contig=<ID=GL000201.1,length=36148>
+##contig=<ID=GL000247.1,length=36422>
+##contig=<ID=GL000245.1,length=36651>
+##contig=<ID=GL000197.1,length=37175>
+##contig=<ID=GL000203.1,length=37498>
+##contig=<ID=GL000246.1,length=38154>
+##contig=<ID=GL000249.1,length=38502>
+##contig=<ID=GL000196.1,length=38914>
+##contig=<ID=GL000248.1,length=39786>
+##contig=<ID=GL000244.1,length=39929>
+##contig=<ID=GL000238.1,length=39939>
+##contig=<ID=GL000202.1,length=40103>
+##contig=<ID=GL000234.1,length=40531>
+##contig=<ID=GL000232.1,length=40652>
+##contig=<ID=GL000206.1,length=41001>
+##contig=<ID=GL000240.1,length=41933>
+##contig=<ID=GL000236.1,length=41934>
+##contig=<ID=GL000241.1,length=42152>
+##contig=<ID=GL000243.1,length=43341>
+##contig=<ID=GL000242.1,length=43523>
+##contig=<ID=GL000230.1,length=43691>
+##contig=<ID=GL000237.1,length=45867>
+##contig=<ID=GL000233.1,length=45941>
+##contig=<ID=GL000204.1,length=81310>
+##contig=<ID=GL000198.1,length=90085>
+##contig=<ID=GL000208.1,length=92689>
+##contig=<ID=GL000191.1,length=106433>
+##contig=<ID=GL000227.1,length=128374>
+##contig=<ID=GL000228.1,length=129120>
+##contig=<ID=GL000214.1,length=137718>
+##contig=<ID=GL000221.1,length=155397>
+##contig=<ID=GL000209.1,length=159169>
+##contig=<ID=GL000218.1,length=161147>
+##contig=<ID=GL000220.1,length=161802>
+##contig=<ID=GL000213.1,length=164239>
+##contig=<ID=GL000211.1,length=166566>
+##contig=<ID=GL000199.1,length=169874>
+##contig=<ID=GL000217.1,length=172149>
+##contig=<ID=GL000216.1,length=172294>
+##contig=<ID=GL000215.1,length=172545>
+##contig=<ID=GL000205.1,length=174588>
+##contig=<ID=GL000219.1,length=179198>
+##contig=<ID=GL000224.1,length=179693>
+##contig=<ID=GL000223.1,length=180455>
+##contig=<ID=GL000195.1,length=182896>
+##contig=<ID=GL000212.1,length=186858>
+##contig=<ID=GL000222.1,length=186861>
+##contig=<ID=GL000200.1,length=187035>
+##contig=<ID=GL000193.1,length=189789>
+##contig=<ID=GL000194.1,length=191469>
+##contig=<ID=GL000225.1,length=211173>
+##contig=<ID=GL000192.1,length=547496>
+##contig=<ID=NC_007605,length=171823>
+#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	HG00141	HG01958	HG01530
+1	12141	.	C	<NON_REF>	.	.	END=12144	GT:GQ:PL:MIN_DP:DP	./.:0:0,0,0:0:2	./.:.:.:.:.	./.:.:.:.:.
+1	12145	.	C	<NON_REF>	.	.	END=12277	GT:GQ:PL:MIN_DP:DP	./.:0:0,0,0:0:2	./.:0:0,0,0:0:3	./.:.:.:.:.
+1	12278	.	C	<NON_REF>	.	.	END=12295	GT:GQ:PL:MIN_DP:DP	./.:0:0,0,0:0:2	./.:.:.:.:.	./.:.:.:.:.
+1	17385	.	G	A,T,<NON_REF>	.	.	BaseQRankSum=-2.074;ClippingRankSum=-1.859;MQRankSum=-0.432;ReadPosRankSum=0.005;MQ=31.72;MQ0=3;DP=828	GT:GQ:SB:AD:PL:PGT:PID:DP	./.:99:58,0,22,0:58,22,17,17:504,0,9807,678,1870,2548,678,1870,2548,2548:0|1:17385_G_A:80	./.:99:0,0,0,0:0,37,120,37:3336,4536,7349,358,958,0,4536,7349,958,7349:0|1:17385_G_T:120	./.:99:9,31,13,23:40,36,0,0:1018,0,1116,1137,1224,2361,1137,1224,2361,2361:.:.:76

tests/run.py
@@ -62,7 +62,7 @@ loader_json_template_string="""
     ],
     "callset_mapping_file" : "",
     "vid_mapping_file" : "inputs/vid.json",
-    "size_per_column_partition": 3000,
+    "size_per_column_partition": 700 ,
     "treat_deletions_as_intervals" : true,
     "vcf_header_filename": "inputs/template_vcf_header.vcf",
     "reference_genome" : "inputs/chr1_10MB.fasta.gz",
@@ -310,6 +310,15 @@ def main():
                         } }
                     ]
             },
+            { "name" : "t0_1_2_combined", 'golden_output' : 'golden_outputs/t0_1_2_combined',
+                'callset_mapping_file': 'inputs/callsets/t0_1_2_combined.json',
+                "query_params": [
+                    { "query_column_ranges" : [0, 1000000000], "golden_output": {
+                        "vcf"        : "golden_outputs/t0_1_2_combined",
+                        "batched_vcf": "golden_outputs/t0_1_2_combined",
+                        } },
+                    ]
+            }
     ];
     for test_params_dict in loader_tests:
         test_name = test_params_dict['name']