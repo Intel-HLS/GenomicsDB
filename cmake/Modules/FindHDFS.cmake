45a69520acac7a0573993c3c6c936e34e85f2441
Melvin Lathara <melvin.lathara@gmail.com>
2018-01-22 23:07:45 +0000

    Fixing up copyright

--- cmake/Modules/FindHDFS.cmake
+++ cmake/Modules/FindHDFS.cmake
@@ -4,7 +4,7 @@
 #
 # The MIT License
 #
-# Copyright (c) 2017 Omics Data Automation, Inc.
+# Copyright 2017 UCLA. License pursuant to original Intel MIT license.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
