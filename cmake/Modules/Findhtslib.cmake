f6bbf21dd6df936268afa9e4fd400dbc04a313ba
Karthik Gururaj <karthik.gururaj@intel.com>
2018-07-08 12:21:33 -0700

    * Updated to latest htslib develop branch * Enable libcurl in htslib build so that VCF file I/O can be performed directly on object stores

--- cmake/Modules/Findhtslib.cmake
+++ cmake/Modules/Findhtslib.cmake
@@ -19,6 +19,12 @@ if(HTSLIB_SOURCE_DIR)
     if((NOT (CMAKE_SYSTEM_NAME STREQUAL CMAKE_HOST_SYSTEM_NAME)) AND APPLE)
         set(HTSLIB_OSXCROSS_COMPILE_FLAGS LIBS=${OSXCROSS_LIBS} CPPFLAGS=${OSXCROSS_CPPFLAGS} --host=${CMAKE_SYSTEM_PROCESSOR}-${CMAKE_SYSTEM})
     endif()
+    if(CURL_FOUND)
+      set(HTSLIB_CURL_FLAGS --enable-libcurl --enable-gcs --enable-s3)
+      set(HTSLIB_${CMAKE_BUILD_TYPE}_CFLAGS "${HTSLIB_${CMAKE_BUILD_TYPE}_CFLAGS} -I${CURL_INCLUDE_DIRS}")
+    else()
+      set(HTSLIB_CURL_FLAGS --disable-libcurl)
+    endif()
     ExternalProject_Add(
         htslib
         DOWNLOAD_COMMAND ""
@@ -28,9 +34,11 @@ if(HTSLIB_SOURCE_DIR)
         CONFIGURE_COMMAND ${HTSLIB_SOURCE_DIR}/configure CFLAGS=${HTSLIB_${CMAKE_BUILD_TYPE}_CFLAGS} LDFLAGS=${HTSLIB_${CMAKE_BUILD_TYPE}_LDFLAGS}
             CC=${CMAKE_C_COMPILER} AR=${CMAKE_AR} RANLIB=${CMAKE_RANLIB}
             ${HTSLIB_OSXCROSS_COMPILE_FLAGS}
-            --disable-lzma --disable-bz2 --disable-libcurl
+            --disable-lzma --disable-bz2
+            ${HTSLIB_CURL_FLAGS}
         BUILD_COMMAND ${CMAKE_COMMAND} -E make_directory cram
             COMMAND ${CMAKE_COMMAND} -E make_directory test
+            COMMAND ${CMAKE_COMMAND} -E copy ${HTSLIB_SOURCE_DIR}/version.sh .
             COMMAND $(MAKE) -f ${HTSLIB_SOURCE_DIR}/Makefile VPATH=${HTSLIB_SOURCE_DIR} SOURCE_DIR=${HTSLIB_SOURCE_DIR}
             AR=${CMAKE_AR}
         #BUILD_IN_SOURCE 1
